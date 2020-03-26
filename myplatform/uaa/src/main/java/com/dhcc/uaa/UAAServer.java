54b441f29f134683da35633ed4ab5fc2e33e4a24
dujianling <dujianling@dhcc.com.cn>
2020-03-26 09:02:44 +0800

    平台提交

---
+++ myplatform/uaa/src/main/java/com/dhcc/uaa/UAAServer.java
@@ -0,0 +1,26 @@
+package com.dhcc.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableHystrix
+@EnableFeignClients(basePackages = "com.dhcc.uaa")
+public class UAAServer {
+
+    public static void main(String[] args){
+
+        SpringApplication.run(UAAServer.class,args);
+
+    }
+
+
+
+
+
+
+}
---
+++ myplatform/uaa/src/main/java/com/dhcc/uaa/config/WebSecurityConfig.java
@@ -0,0 +1,47 @@
+package com.dhcc.uaa.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * Spring Security默认是禁用注解的，要想开启注解， 需要在继承WebSecurityConfigurerAdapter的类上
+ * 加@EnableGlobalMethodSecurity注解， 来判断用户对某个控制层的方法是否具有访问权限 
+ */
+@Configuration
+@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    //认证管理器
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    //密码编码器
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    //安全拦截机制（最重要）
+/*    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/r/r1").hasAnyAuthority("p1")
+                .antMatchers("/login*").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+    }*/
+
+
+}
